ac46ec68edf5ae6de9063f2a29a1ae5ea3c2b530
Akshat Ramanathan <ramanathan.ak@northeastern.edu>
2022-09-18 10:12:18 -0400

    project init commit

---
+++ src/dao/UsersDetails.java
@@ -0,0 +1,198 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.awt.Image;
+import java.util.Date;
+
+/**
+ *
+ * @author akshb
+ */
+public class UsersDetails {
+    private String firstName;
+    private String lastName;
+    private Date dob;
+    private int age;
+    private long telNumber;
+    private String email;
+    private String addr1;
+    private String addr2;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String degree1;
+    private Date deg1Start;
+    private Date deg1End;
+    private String degree2;
+    private Date deg2Start;
+    private Date deg2End;
+    private Image pic;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setTelNumber(long telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddr1(String addr1) {
+        this.addr1 = addr1;
+    }
+
+    public void setAddr2(String addr2) {
+        this.addr2 = addr2;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public void setDegree1(String degree1) {
+        this.degree1 = degree1;
+    }
+
+    public void setDeg1Start(Date deg1Start) {
+        this.deg1Start = deg1Start;
+    }
+
+    public void setDeg1End(Date deg1End) {
+        this.deg1End = deg1End;
+    }
+
+    public void setDegree2(String degree2) {
+        this.degree2 = degree2;
+    }
+
+    public void setDeg2Start(Date deg2Start) {
+        this.deg2Start = deg2Start;
+    }
+
+    public void setDeg2End(Date deg2End) {
+        this.deg2End = deg2End;
+    }
+
+    public void setPic(Image pic) {
+        this.pic = pic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getTelNumber() {
+        return telNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddr1() {
+        return addr1;
+    }
+
+    public String getAddr2() {
+        return addr2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public String getDegree1() {
+        return degree1;
+    }
+
+    public Date getDeg1Start() {
+        return deg1Start;
+    }
+
+    public Date getDeg1End() {
+        return deg1End;
+    }
+
+    public String getDegree2() {
+        return degree2;
+    }
+
+    public Date getDeg2Start() {
+        return deg2Start;
+    }
+
+    public Date getDeg2End() {
+        return deg2End;
+    }
+
+    public Image getPic() {
+        return pic;
+    }
+    
+    
+    
+}
